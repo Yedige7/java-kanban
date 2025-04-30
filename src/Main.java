9b26139d1d1a31836db0cdb644f13d9127e67f13
Yedige7 <Yedige7@gmail.com>
2025-05-01 02:09:01 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import ru.yandex.practicum.tracker.*;
 
-import static ru.yandex.practicum.tracker.Status.DONE;
-import static ru.yandex.practicum.tracker.Status.IN_PROGRESS;
-
 public class Main {
 
     public static void main(String[] args) {
